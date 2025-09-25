7c8b86a9b8a62734776d8210a0992c0bb9d6ec5e
ABAKHAR721 <abdssamad.abkhar22@gmail.com>
2025-09-25 16:12:22 +0100

    try to fix events issus

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { Toaster } from "@/components/ui/toaster";
-
+ 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
