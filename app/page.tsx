3b2ae7b5176f7053bcebb98942bd0a33d709562e
Yassine.io <yassinemazhare@gmail.com>
2025-09-30 20:27:16 +0100

    update Home component to include Annonce and PizzaMomentPreview, enhancing the layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,20 @@
 import Header from '@/components/Header/Header';
 import HomeSlider from '@/components/Home-Slider/HomeSlider';
+import Annonce from '@/components/Header/Annonce';
 import PizzaMoment from '@/components/pizza-moment/PizzaMoment';
+import PizzaMomentPreview from '@/components/pizza-moment/PizzaMomentPreview';
 import EventsPromo from '@/components/events-promo/EventsPromo';
 import Footer from '@/components/footer/Footer';
+import FooterPreview from '@/components/footer/FooterPreview';
 
 export default function Home() {
   return ( 
     
     <main>
-      {/* Set showAnnonce to true or false depending on your needs */}
-      <Header showAnnonce={true} />
-      <HomeSlider /> 
-      <PizzaMoment />
+      <Header />
+      <HomeSlider />
+      <Annonce />
+      <PizzaMomentPreview />
       <EventsPromo />
       <Footer />
     </main>
